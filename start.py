c06e72ab181e318a3bcf4701444698add46d4b76
cooklee <sbbogus@gmail.com>
2021-01-28 11:55:23 +0100

    extends szablon

--- start.py
+++ start.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def main():
     return render_template("base.html")
 
+@app.route('/add_authors')
+def author():
+    return render_template("author.html")
+
 if __name__ == '__main__':
     app.run()
